1b79e440b089c485cb4817eda019cab19b890123
erlindiisaj <erlind.isaj02@gmail.com>
2020-02-11 19:25:23 +0100

    Resposive

--- js/script.js
+++ js/script.js
@@ -13,17 +13,24 @@ const icon3 = document.getElementById('git')
 const icon4 = document.getElementById('goo')
 const bars = document.getElementById('fa-bars')
 
+const icon19 = document.getElementById('fb1')
+const icon11 = document.getElementById('tw1')
+const icon21 = document.getElementById('in1')
+const icon31 = document.getElementById('git1')
+const icon41 = document.getElementById('goo1')
+
 
 
 var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    addClasses();
-  } else {
-    removeClasses();
-  }
-}
+// $(document).ready(function() {
+	function myFunction() {
+	  if (window.pageYOffset > sticky) {
+		addClasses();
+	  } else {
+		removeClasses();
+	  }
+	}
+// })
 
 const addClasses = () => {
   navbar.classList.add("sticky");
@@ -58,112 +65,79 @@ particlesJS.load('particles-js-2', 'particlesjs-2.json');
 
 /////////////////////////////////////////////
 
-// List of sentences
-var _CONTENT = [ 
-	"I'm Web Developer.", 
-	"I'm not a great programmer", 
-	"I'm just a good programmer", 
-	"with great habits."
-];
+	// List of sentences
+	var _CONTENT = [ 
+		"I'm Web Developer.", 
+		"I'm not a great programmer", 
+		"I'm just a good programmer", 
+		"with great habits."
+	];
 
-// Current sentence being processed
-var _PART = 0;
+	// Current sentence being processed
+	var _PART = 0;
 
-// Character number of the current sentence being processed 
-var _PART_INDEX = 0;
+	// Character number of the current sentence being processed 
+	var _PART_INDEX = 0;
 
-// Holds the handle returned from setInterval
-var _INTERVAL_VAL;
+	// Holds the handle returned from setInterval
+	var _INTERVAL_VAL;
 
-// Element that holds the text
-var _ELEMENT = document.querySelector("#text");
+	// Element that holds the text
+	var _ELEMENT = document.querySelector("#text");
 
-// Cursor element 
-var _CURSOR = document.querySelector("#cursor");
+	// Cursor element 
+	var _CURSOR = document.querySelector("#cursor");
 
-// Implements typing effect
-function Type() { 
-	// Get substring with 1 characater added
-	var text =  _CONTENT[_PART].substring(0, _PART_INDEX + 1);
-	_ELEMENT.innerHTML = text;
-	_PART_INDEX++;
+	// Implements typing effect
+	function Type() { 
+		// Get substring with 1 characater added
+		var text =  _CONTENT[_PART].substring(0, _PART_INDEX + 1);
+		_ELEMENT.innerHTML = text;
+		_PART_INDEX++;
 
-	// If full sentence has been displayed then start to delete the sentence after some time
-	if(text === _CONTENT[_PART]) {
-		// Hide the cursor
-		_CURSOR.style.display = 'none';
+		// If full sentence has been displayed then start to delete the sentence after some time
+		if(text === _CONTENT[_PART]) {
+			// Hide the cursor
+			_CURSOR.style.display = 'none';
 
-		clearInterval(_INTERVAL_VAL);
-		setTimeout(function() {
-			_INTERVAL_VAL = setInterval(Delete, 50);
-		}, 2000);
+			clearInterval(_INTERVAL_VAL);
+			setTimeout(function() {
+				_INTERVAL_VAL = setInterval(Delete, 50);
+			}, 2000);
+		}
 	}
-}
 
-// Implements deleting effect
-function Delete() {
-	// Get substring with 1 characater deleted
-	var text =  _CONTENT[_PART].substring(0, _PART_INDEX - 1);
-	_ELEMENT.innerHTML = text;
-	_PART_INDEX--;
+	// Implements deleting effect
+	function Delete() {
+		// Get substring with 1 characater deleted
+		var text =  _CONTENT[_PART].substring(0, _PART_INDEX - 1);
+		_ELEMENT.innerHTML = text;
+		_PART_INDEX--;
 
-	// If sentence has been deleted then start to display the next sentence
-	if(text === '') {
-		clearInterval(_INTERVAL_VAL);
+		// If sentence has been deleted then start to display the next sentence
+		if(text === '') {
+			clearInterval(_INTERVAL_VAL);
 
-		// If current sentence was last then display the first one, else move to the next
-		if(_PART == (_CONTENT.length - 1))
-			_PART = 0;
-		else
-			_PART++;
-		
-		_PART_INDEX = 0;
+			// If current sentence was last then display the first one, else move to the next
+			if(_PART == (_CONTENT.length - 1))
+				_PART = 0;
+			else
+				_PART++;
+			
+			_PART_INDEX = 0;
 
-		// Start to display the next sentence after some time
-		setTimeout(function() {
-			_CURSOR.style.display = 'inline-block';
-			_INTERVAL_VAL = setInterval(Type, 100);
-		}, 200);
+			// Start to display the next sentence after some time
+			setTimeout(function() {
+				_CURSOR.style.display = 'inline-block';
+				_INTERVAL_VAL = setInterval(Type, 100);
+			}, 200);
+		}
 	}
-}
 
-// Start the typing effect on load
-_INTERVAL_VAL = setInterval(Type, 100);
+	// Start the typing effect on load
+	_INTERVAL_VAL = setInterval(Type, 100);
 
-
-
-
-function animateValue(id, start, end, duration) {
-    // assumes integer values for start and end
-    
-    var obj = document.getElementById(id);
-    var range = end - start;
-    // no timer shorter than 50ms (not really visible any way)
-    var minTimer = 100;
-    // calc step time to show all interediate values
-    var stepTime = Math.abs(Math.floor(duration / range));
-    
-    // never go below minTimer
-    stepTime = Math.max(stepTime, minTimer);
-    
-    // get current time and calculate desired end time
-    var startTime = new Date().getTime();
-    var endTime = startTime + duration;
-    var timer;
-  
-    function run() {
-        var now = new Date().getTime();
-        var remaining = Math.max((endTime - now) / duration, 0);
-        var value = Math.round(end - (remaining * range));
-        obj.innerHTML = value;
-        if (value == end) {
-            clearInterval(timer);
-        }
-    }
-    
-    timer = setInterval(run, stepTime);
-    run();
-}
+//////////////////////////////////////////////////
 
 
 
@@ -172,7 +146,7 @@ let isY = false;
 let executed = false;
 window.addEventListener('scroll', () => {
 	const scrollY = window.scrollY;
-	if (scrollY >= 1050 && scrollY <= 2000) {
+	if (scrollY >= 1050 && scrollY <= 2400) {
 		isY = true; 
 	} else {
 		isY = false;
@@ -180,18 +154,26 @@ window.addEventListener('scroll', () => {
 	if (!executed) {
 		if (isY) {
 			executed = true;
-			animateValue("counter", 0, 448, 3000);
-			animateValue("counter1", 0, 50, 3000);
-			animateValue("counter2", 2790, 3540, 3000);
-			animateValue("counter3", 0, 20, 3000);
+			$('.count').each(function () {
+				$(this).prop('Counter',0).animate({
+					Counter: $(this).text()
+				}, {
+					duration: 4000,
+					easing: 'swing',
+					step: function (now) {
+						$(this).text(Math.ceil(now));
+					}
+				});
+			});
 		}
 	}
 })
 
 
 $(document).ready(function() {
+
 		const scrollY = window.scrollY;
-		if (scrollY >= 1050 && scrollY <= 2000) {
+		if (scrollY >= 1050 && scrollY <= 2400) {
 			isY = true; 
 		} else {
 			isY = false;
@@ -199,10 +181,17 @@ $(document).ready(function() {
 		if (!executed) {
 			if (isY) {
 				executed = true;
-				animateValue("counter", 0, 448, 3000);
-				animateValue("counter1", 0, 50, 3000);
-				animateValue("counter2", 2790, 3540, 3000);
-				animateValue("counter3", 0, 20, 3000);
+				$('.count').each(function () {
+					$(this).prop('Counter',0).animate({
+						Counter: $(this).text()
+					}, {
+						duration: 4000,
+						easing: 'swing',
+						step: function (now) {
+							$(this).text(Math.ceil(now));
+						}
+					});
+				});
 			}
 		}
    });
@@ -221,12 +210,22 @@ $(window).resize(function() {
 		icon2.classList.remove('fa-lg')
 		icon3.classList.remove('fa-lg')
 		icon4.classList.remove('fa-lg')
+		icon19.classList.remove('fa-2x')
+		icon11.classList.remove('fa-2x')
+		icon21.classList.remove('fa-2x')
+		icon31.classList.remove('fa-2x')
+		icon41.classList.remove('fa-2x')
 	} else {
 		icon.classList.add('fa-lg')
 		icon1.classList.add('fa-lg')
 		icon2.classList.add('fa-lg')
 		icon3.classList.add('fa-lg')
 		icon4.classList.add('fa-lg')
+		icon19.classList.add('fa-2x')
+		icon11.classList.add('fa-2x')
+		icon21.classList.add('fa-2x')
+		icon31.classList.add('fa-2x')
+		icon41.classList.add('fa-2x')
 	}
 });
 
@@ -238,12 +237,22 @@ $(document).ready(function(){
 		icon2.classList.remove('fa-lg')
 		icon3.classList.remove('fa-lg')
 		icon4.classList.remove('fa-lg')
+		icon19.classList.remove('fa-2x')
+		icon11.classList.remove('fa-2x')
+		icon21.classList.remove('fa-2x')
+		icon31.classList.remove('fa-2x')
+		icon41.classList.remove('fa-2x')
 	} else {
 		icon.classList.add('fa-lg')
 		icon1.classList.add('fa-lg')
 		icon2.classList.add('fa-lg')
 		icon3.classList.add('fa-lg')
 		icon4.classList.add('fa-lg')
+		icon19.classList.add('fa-2x')
+		icon11.classList.add('fa-2x')
+		icon21.classList.add('fa-2x')
+		icon31.classList.add('fa-2x')
+		icon41.classList.add('fa-2x')
 	}
 	  linkScroll = $('.scroll');
   
