d492223062cb57322609c504e375ed538aa9f2c6
erlindiisaj <erlind.isaj02@gmail.com>
2020-02-26 18:26:26 +0100

    Fix

--- js/script.js
+++ js/script.js
@@ -1,33 +1,39 @@
-window.onscroll = function() {myFunction()};
-const links = document.querySelectorAll('.nav-links li')
+const links = document.querySelectorAll('.nav-links li');
+const bars = document.getElementById('fa-bars;');
+const logo = document.getElementById('logo-div');
 var navbar = document.getElementById("navbar");
 const link1 = document.getElementById('link1');
 const link2 = document.getElementById('link2');
 const link3 = document.getElementById('link3');
 const link4 = document.getElementById('link4');
-const logo = document.getElementById('logo-div');
 const logoP = document.getElementById('logoP');
-const icon = document.getElementById('fb')
-const icon1 = document.getElementById('tw')
-const icon2 = document.getElementById('in')
-const icon3 = document.getElementById('git')
-const icon4 = document.getElementById('goo')
-const bars = document.getElementById('fa-bars')
-const icon19 = document.getElementById('fb1')
-const icon11 = document.getElementById('tw1')
-const icon21 = document.getElementById('in1')
-const icon31 = document.getElementById('git1')
-const icon41 = document.getElementById('goo1')
-
-
-
+const icon41 = document.getElementById('goo1');
+const icon31 = document.getElementById('git1');
+const icon19 = document.getElementById('fb1');
+const icon21 = document.getElementById('in1');
+const icon11 = document.getElementById('tw1');
+const icon3 = document.getElementById('git');
+const icon4 = document.getElementById('goo');
+const icon2 = document.getElementById('in');
+const icon1 = document.getElementById('tw');
+const icon = document.getElementById('fb');
+let sticky = navbar.offsetTop;
+let executed = false;
+let isY = false;
 
 particlesJS.load('particles-js', 'particlesjs.json');
 particlesJS.load('particles-js-2', 'particlesjs-2.json');
 
-var sticky = navbar.offsetTop;
 
-function myFunction() {
+
+
+///////////////////////////////////////
+///////////  FUNCTIONS  ///////////////
+///////////////////////////////////////
+
+
+
+const myFunction = () => {
 	if (window.pageYOffset > sticky) {
 		addClasses();
 	} else {
@@ -37,7 +43,6 @@ function myFunction() {
 
 const addClasses = () => {
   navbar.classList.add("sticky");
- 
   link1.classList.add("link-sticky");
   link2.classList.add("link-sticky");
   link3.classList.add("link-sticky");
@@ -45,9 +50,6 @@ const addClasses = () => {
   logoP.classList.remove('opc1');
   bars.classList.add('fa-bars-1');
   logoP.classList.add('logo-p-1')
-  
-  
-
 };
 
 const removeClasses = () => {
@@ -61,6 +63,156 @@ const removeClasses = () => {
   logoP.classList.remove('logo-p-1')
 
 };
+
+const counting = () => {
+	const scrollY = window.scrollY;
+	if (scrollY >= 1050 && scrollY <= 2400) {
+		isY = true; 
+	} else {
+		isY = false;
+	}
+	if (!executed) {
+		if (isY) {
+			executed = true;
+			$('.count').each(function () {
+				$(this).prop('Counter',0).animate({
+					Counter: $(this).text()
+				}, {
+					duration: 4000,
+					easing: 'swing',
+					step: function (now) {
+						$(this).text(Math.ceil(now));
+					}
+				});
+			});
+		}
+	}
+};
+
+const resIcon = () => {
+	let w = $(window).width();
+	if(w<1024) {
+		icon.classList.remove('fa-lg')
+		icon1.classList.remove('fa-lg')
+		icon2.classList.remove('fa-lg')
+		icon3.classList.remove('fa-lg')
+		icon4.classList.remove('fa-lg')
+		icon19.classList.remove('fa-2x')
+		icon11.classList.remove('fa-2x')
+		icon21.classList.remove('fa-2x')
+		icon31.classList.remove('fa-2x')
+		icon41.classList.remove('fa-2x')
+	} else {
+		icon.classList.add('fa-lg')
+		icon1.classList.add('fa-lg')
+		icon2.classList.add('fa-lg')
+		icon3.classList.add('fa-lg')
+		icon4.classList.add('fa-lg')
+		icon19.classList.add('fa-2x')
+		icon11.classList.add('fa-2x')
+		icon21.classList.add('fa-2x')
+		icon31.classList.add('fa-2x')
+		icon41.classList.add('fa-2x')
+	}
+	  linkScroll = $('.scroll');
+  
+  linkScroll.click(function(e){
+      e.preventDefault();
+      $('body, html').animate({
+         scrollTop: $(this.hash).offset().top
+      }, 900);
+   });
+};
+
+const active = () => {
+	if (window.pageYOffset > sticky) {
+			$(".nav-div ul").toggleClass("active");
+			
+		} else {
+			$('#logoP').toggleClass('logo-p-1');
+			$(".nav-div ul").toggleClass("active");
+			$(".fa-bars").toggleClass('fa-bars-1');
+		}
+};
+
+const lockScroll = () => {
+	if ($('body').hasClass('lock-scroll')) {
+		$('body').removeClass('lock-scroll');
+	}
+	else {
+		$('body').addClass('lock-scroll');
+
+	}
+};
+
+
+
+///////////////////////////////////////
+///////////             ///////////////
+///////////////////////////////////////
+
+
+
+
+
+$(window).resize(function() {
+	resIcon();
+});
+
+
+
+$(window).on('scroll', function(){
+	counting();
+	myFunction();
+	if ($('.nav-div ul').hasClass("active")) {
+		if ($('.fa-bars').hasClass("fa-bars-1")) {
+			
+		} else {
+			$(".fa-bars").toggleClass('fa-bars-1');
+			$('#logoP').toggleClass('logo-p-1');
+		}
+	}
+
+});
+
+
+$(document).ready(function(){
+	resIcon();
+	counting();
+	$(".fa-bars").click(function(){
+		active();
+		lockScroll();
+	})
+});
+
+
+
+
+$(".navigation a").click(function () {
+	$(".nav-div ul").removeClass('active');
+	lockScroll();
+});
+
+$('#ul').click(function () {
+	$(".nav-div ul").removeClass('active');
+	lockScroll();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -146,189 +298,6 @@ const removeClasses = () => {
 /////////////////////////////////////////////
 //////////   Content Typing   ///////////////
 /////////////////////////////////////////////
-
-
-
-
-let isY = false;
-let executed = false;
-
-
-window.addEventListener('scroll', () => {
-	const scrollY = window.scrollY;
-	if (scrollY >= 1050 && scrollY <= 2400) {
-		isY = true; 
-	} else {
-		isY = false;
-	}
-	if (!executed) {
-		if (isY) {
-			executed = true;
-			$('.count').each(function () {
-				$(this).prop('Counter',0).animate({
-					Counter: $(this).text()
-				}, {
-					duration: 4000,
-					easing: 'swing',
-					step: function (now) {
-						$(this).text(Math.ceil(now));
-					}
-				});
-			});
-		}
-	}
-});
-
-
-$(document).ready(function() {
-
-		const scrollY = window.scrollY;
-		if (scrollY >= 1050 && scrollY <= 2400) {
-			isY = true; 
-		} else {
-			isY = false;
-		}
-		if (!executed) {
-			if (isY) {
-				executed = true;
-				$('.count').each(function () {
-					$(this).prop('Counter',0).animate({
-						Counter: $(this).text()
-					}, {
-						duration: 4000,
-						easing: 'swing',
-						step: function (now) {
-							$(this).text(Math.ceil(now));
-						}
-					});
-				});
-			}
-		}
-});
-
-
-$(window).resize(function() {
-	let w = $(window).width();
-
-	
-	if(w<1024) {
-		icon.classList.remove('fa-lg')
-		icon1.classList.remove('fa-lg')
-		icon2.classList.remove('fa-lg')
-		icon3.classList.remove('fa-lg')
-		icon4.classList.remove('fa-lg')
-		icon19.classList.remove('fa-2x')
-		icon11.classList.remove('fa-2x')
-		icon21.classList.remove('fa-2x')
-		icon31.classList.remove('fa-2x')
-		icon41.classList.remove('fa-2x')
-	} else {
-		icon.classList.add('fa-lg')
-		icon1.classList.add('fa-lg')
-		icon2.classList.add('fa-lg')
-		icon3.classList.add('fa-lg')
-		icon4.classList.add('fa-lg')
-		icon19.classList.add('fa-2x')
-		icon11.classList.add('fa-2x')
-		icon21.classList.add('fa-2x')
-		icon31.classList.add('fa-2x')
-		icon41.classList.add('fa-2x')
-	}
-});
-
-
-$(document).ready(function(){
-	let w = $(window).width();
-	if(w<1024) {
-		icon.classList.remove('fa-lg')
-		icon1.classList.remove('fa-lg')
-		icon2.classList.remove('fa-lg')
-		icon3.classList.remove('fa-lg')
-		icon4.classList.remove('fa-lg')
-		icon19.classList.remove('fa-2x')
-		icon11.classList.remove('fa-2x')
-		icon21.classList.remove('fa-2x')
-		icon31.classList.remove('fa-2x')
-		icon41.classList.remove('fa-2x')
-	} else {
-		icon.classList.add('fa-lg')
-		icon1.classList.add('fa-lg')
-		icon2.classList.add('fa-lg')
-		icon3.classList.add('fa-lg')
-		icon4.classList.add('fa-lg')
-		icon19.classList.add('fa-2x')
-		icon11.classList.add('fa-2x')
-		icon21.classList.add('fa-2x')
-		icon31.classList.add('fa-2x')
-		icon41.classList.add('fa-2x')
-	}
-	  linkScroll = $('.scroll');
-  
-  linkScroll.click(function(e){
-      e.preventDefault();
-      $('body, html').animate({
-         scrollTop: $(this.hash).offset().top
-      }, 900);
-   });
-});
-
-
-
-
-$(window).on('scroll', function(){
-	if ($('.nav-div ul').hasClass("active")) {
-		if ($('.fa-bars').hasClass("fa-bars-1")) {
-			
-		} else {
-			$(".fa-bars").toggleClass('fa-bars-1');
-			$('#logoP').toggleClass('logo-p-1');
-		}
-	}
-
-});
-
-const active = () => {
-	if (window.pageYOffset > sticky) {
-			$(".nav-div ul").toggleClass("active");
-			
-		} else {
-			$('#logoP').toggleClass('logo-p-1');
-			$(".nav-div ul").toggleClass("active");
-			$(".fa-bars").toggleClass('fa-bars-1');
-		}
-}
-
-const lockScroll = () => {
-	if ($('body').hasClass('lock-scroll')) {
-		$('body').removeClass('lock-scroll');
-	}
-	else {
-		$('body').addClass('lock-scroll');
-
-	}
-}
-
-
-$(document).ready(function(){
-	$(".fa-bars").click(function(){
-		active();
-		lockScroll();
-		
-	})
-});
-
-$(".navigation a").click(function () {
-	$(".nav-div ul").removeClass('active');
-	lockScroll();
-});
-
-$('#ul').click(function () {
-	$(".nav-div ul").removeClass('active');
-	lockScroll();
-});
-
-
-
 
 
 
