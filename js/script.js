a3f6dda48a6e740036755b0e6040491b725f6b40
erlindiisaj <erlind.isaj02@gmail.com>
2022-07-29 20:41:07 +0200

    Res fix

--- js/script.js
+++ js/script.js
@@ -195,18 +195,19 @@ let startX;
 let scrollLeft;
 
 slider.addEventListener('mousedown', (e) => {
+  event.preventDefault();
   isDown = true;
-  slider.classList.add('active');
+  slider.classList.add('active1');
   startX = e.pageX - slider.offsetLeft;
   scrollLeft = slider.scrollLeft;
 });
 slider.addEventListener('mouseleave', () => {
   isDown = false;
-  slider.classList.remove('active');
+  slider.classList.remove('active1');
 });
 slider.addEventListener('mouseup', () => {
   isDown = false;
-  slider.classList.remove('active');
+  slider.classList.remove('active1');
 });
 slider.addEventListener('mousemove', (e) => {
   if (!isDown) return;
